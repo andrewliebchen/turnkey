c2f84b5f48fbe508d6dcdaa3c091dfe9ca46f0c4
Andrew Liebchen <liebchen@fb.com>
2019-12-17 11:46:37 -0800

    Display chats for the selected person

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { people, chats } from "./data";
 import store from "./store";
 import { view } from "react-easy-state";
 
-console.log(people);
-console.log(chats);
-
 const App = () => {
-  let selectedPerson = people.find(person => store.selectedId === person.id);
+  let selectedPerson = store.people.find(
+    person => store.selectedId === person.id
+  );
+  let selectedChats = store.chats.filter(
+    chat => store.selectedId === chat.parentId
+  );
   return (
     <div>
       <h3>People</h3>
       <ul>
-        {people.map(person => (
+        {store.people.map(person => (
           <li key={person.id}>
             <img src={person.image} alt="avatar" height={24} width={24} />
             {person.firstName} {person.lastName}
@@ -46,7 +47,16 @@ const App = () => {
 
       <hr />
       <h3>Chat</h3>
-      <p>Select a person</p>
+      {selectedChats.length > 0 && (
+        <ul>
+          {selectedChats.map(chat => (
+            <li key={chat.id}>
+              <small>{chat.createdBy}</small>
+              <p>{chat.text}</p>
+            </li>
+          ))}
+        </ul>
+      )}
       <input type="text" placeholder="Add a comment" />
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { store } from "react-easy-state";
+import { people, chats } from "./data";
 
 export default store({
+  people: people,
+  chats: chats,
   selectedId: null
 });
