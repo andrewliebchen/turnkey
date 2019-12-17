3293865b0b1a28231ca4c3a31b9b1ffe8f66f973
Andrew Liebchen <liebchen@fb.com>
2019-12-17 07:03:26 -0800

    Use faker schema to generate fake data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import people from "./data";
+import { people } from "./data";
+
+console.log(people);
 
 const App = () => (
   <div>
     <ul>
       {people.map(person => (
-        <li key={person.name}>{person.name}</li>
+        <li key={person.id}>
+          <img src={person.image} alt="avatar" height={24} width={24} />
+          {person.firstName} {person.lastName}
+        </li>
       ))}
     </ul>
   </div>
--- src/data.js
+++ src/data.js
@@ -1,8 +1,14 @@
-export default [
-  {
-    name: "Andrew Liebechen"
-  },
-  {
-    name: "Marta Crowe"
-  }
-];
+import faker from "faker";
+import { Schema } from "faker-schema";
+import Ids from "ids";
+
+const ids = new Ids();
+
+const personSchema = new Schema(() => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  image: faker.image.avatar(),
+  id: ids.next()
+}));
+
+export const people = personSchema.make(5);
